a23c56c64baea07625fcb53816bfad621193dfd0
rNom <reo1311090912@gmail.com>
2023-11-29 19:01:40 +0900

    ArtSpaceScreen雛形作成

---
+++ ArtSpaceApp/app/src/main/java/com/example/artspaceapp/ui/ArtSpaceScreen.kt
@@ -0,0 +1,25 @@
+package com.example.artspaceapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ArtSpaceScreen() {
+
+}
+
+@Composable
+fun ImageSection() {
+
+}
+
+@Composable
+fun TileSection() {
+
+}
+
+@Preview
+@Composable
+private fun ArtSpaceScreenPreview() {
+    ArtSpaceScreen()
+}
