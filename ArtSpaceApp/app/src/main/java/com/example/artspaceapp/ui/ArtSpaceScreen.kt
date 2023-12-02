962f0c18121a1dfb7e696dac2b1dc7b6ae2650d2
rNom <reo1311090912@gmail.com>
2023-12-02 13:31:56 +0900

    TitleSection実装

--- ArtSpaceApp/app/src/main/java/com/example/artspaceapp/ui/ArtSpaceScreen.kt
+++ ArtSpaceApp/app/src/main/java/com/example/artspaceapp/ui/ArtSpaceScreen.kt
@@ -1,5 +1,7 @@
 package com.example.artspaceapp.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,8 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.artspaceapp.R
 
 @Composable
 fun ArtSpaceScreen() {
@@ -23,8 +30,33 @@ fun ImageSection() {
 }
 
 @Composable
-fun TileSection() {
-
+fun TitleSection(
+    titleText: String,
+    artistName: String,
+    year: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .background(
+                color = Color.Gray
+            ),
+        horizontalAlignment = Alignment.Start,
+    ) {
+        Text(
+            text = titleText,
+            fontSize = 30.sp,
+            modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)
+        )
+        Row {
+            Text(
+                text = artistName,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp, start = 16.dp)
+            )
+            Text(text = "（$year）")
+        }
+    }
 }
 
 @Composable
@@ -42,16 +74,16 @@ fun ButtonSection(
             onClick = onCLickPreviousButton,
             modifier = Modifier
                 .weight(1f)
-            ) {
-            Text(text = "Previous")
+        ) {
+            Text(text = stringResource(R.string.label_previous))
         }
         Spacer(modifier = Modifier.padding(10.dp))
         Button(
             onClick = onClickNextButton,
             modifier = Modifier
                 .weight(1f)
-            ) {
-            Text(text = "Next")
+        ) {
+            Text(text = stringResource(R.string.label_next))
         }
     }
 }
@@ -59,9 +91,22 @@ fun ButtonSection(
 @Preview
 @Composable
 private fun ArtSpaceScreenPreview() {
-    ButtonSection(
-        modifier = Modifier,
-        onCLickPreviousButton = { /*TODO*/ },
-        onClickNextButton = {}
-    )
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+        TitleSection(
+            titleText = "ArtWorkTitle",
+            artistName = "ArtistName",
+            year = "year",
+            modifier = Modifier
+                .padding(vertical = 24.dp, horizontal = 16.dp)
+                .fillMaxWidth()
+        )
+        ButtonSection(
+            modifier = Modifier,
+            onCLickPreviousButton = {},
+            onClickNextButton = {}
+        )
+    }
 }
